05a0cc4d57a069a52e4baabeb298cfa92907055f
Marius Küng <apps@mariuskueng.ch>
2015-01-14 10:09:38 +0100

    Updated bin files

---
+++ src/project_3/Plot.java
@@ -0,0 +1,13 @@
+package project_3;
+
+public class Plot {
+
+    public Plot() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+    }
+
+}
