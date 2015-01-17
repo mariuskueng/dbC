e5517b37796e981ea2aaee6446c2b8b697fa62ff
Marius Küng <apps@mariuskueng.ch>
2015-01-17 15:51:27 +0100

    Project 4 task
    Replaced mysql with db4o

--- Project_4/src/MovieTests.java
+++ Project_4/src/MovieTests.java
@@ -2,8 +2,14 @@
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
 
 import org.junit.Test;
 import org.hibernate.*;
@@ -18,7 +24,7 @@ import org.junit.BeforeClass;
 
 public class MovieTests {
     /** This is the one session we work on */
-    Session session;
+	ObjectContainer db;
     
     Movie classUnderTest;
     
@@ -34,81 +40,53 @@ public class MovieTests {
     
     @Before
     public void setUp () {
-        // reset session - drops tables
-        session = createSession();
-        
+    	File file = new File("Movie.yap");
+		if(file.exists()){
+			file.delete();
+		}
+    	
+    	db = Db4oEmbedded.openFile("Movie.yap");
+    	
         // setup test data:
         // so every test case has the 
         // same environment work with.
-        createMovieTestEnvironment();
-        
-        // convenience - begins a
-        // new transaction and commits
-        // it in the tearDown method.
-        session.beginTransaction();
+        createMovieTestEnvironment();        
     }
     
     @After
     public void tearDown () {
-        // corresponding part to beginTransaction 
-        // see setup - change simultaneously!
-        session.getTransaction().commit();
-    }
-    
-    public Session createSession () {
-        
-        Configuration config = new Configuration();
-        
-        // add all class annotations
-        config.addAnnotatedClass(MoviePerson.class);
-        config.addAnnotatedClass(Actor.class);
-        config.addAnnotatedClass(Director.class);
-        config.addAnnotatedClass(Movie.class);
-        config.addAnnotatedClass(Screenwriter.class);
-        config.addAnnotatedClass(Screenplay.class);
-        config.configure("hibernate.cfg.xml");
-        
-        // some exception handling here would be awesome
-        new SchemaExport(config).create(true,true);
-        
-        ServiceRegistry serviceRegistry =
-            new StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
-        SessionFactory factory = config.buildSessionFactory(serviceRegistry);
-
-        // first one should be openSession and not getSession
-        // see: https://stackoverflow.com/questions/2378572/hibernate-session-is-closed
-        return factory.openSession();   
+    	// close db connection
+    	db.close();
     }
     
     public void createMovieTestEnvironment () {
-        session.beginTransaction();
-        
+               
         // first we instantiate a actors
         Actor actorMatthew = new Actor("A01", "Matthew", "McConaughey", 45);
         Actor actorAnne = new Actor("A02", "Anne", "Hathaway", 32);
         Actor actorJessica = new Actor("A03", "Jessica", "Chastain", 37);
         
-        session.save(actorMatthew);
-        session.save(actorAnne);
-        session.save(actorJessica);
+        db.store(actorMatthew);
+        db.store(actorAnne);
+        db.store(actorJessica);
         
         // and the director
         Director directorChris = new Director("D01", "Christopher", "Nolan", 44);
         
         // add the director to the database
-        session.save(directorChris);
+        db.store(directorChris);
         
         // and a screenwriter
         
         Screenwriter screenwriter = new Screenwriter("SW01", "Jonathan", "Nolan", 37);
         
-        session.save(screenwriter);
+        db.store(screenwriter);
         
         // and a screenplay 
         
         Screenplay screenplay = new Screenplay("SP01", "Interstallar-2014", Calendar.getInstance(), screenwriter);
         
-        session.save(screenplay);
+        db.store((Screenplay) screenplay);
         
         // and of course a movie
         classUnderTest = new Movie("MINTER", "Interstellar", directorChris, screenplay);
@@ -116,24 +94,28 @@ public class MovieTests {
         classUnderTest.addActor(actorAnne);
         classUnderTest.addActor(actorJessica);
         
-        session.save(classUnderTest);
-        
-        session.getTransaction().commit();
+        db.store(classUnderTest);
+    }
+    
+    public <T> List<T> createTypedList (ObjectSet<T> set) {
+    	List<T> list = new ArrayList<T>();
+    	for (T obj : set) {
+        	list.add((T) obj);
+		}
+    	return list;
     }
     
     @Test
-    @SuppressWarnings("unchecked")
     public void testGetDirectors () {
-        List<Director> allDirectors = session.createQuery("from Director").list();
+    	List<Director> allDirectors = createTypedList(db.query(Director.class));
  
         // check if the director is in the database 
         assertTrue(allDirectors.contains(((Director) new Director("D01", "Christopher", "Nolan", 44))));
     }
     
     @Test
-    @SuppressWarnings("unchecked")
     public void testGetActors () {
-        List<Actor> allActors = session.createQuery("from Actor").list();
+    	List<Actor> allActors = createTypedList(db.query(Actor.class));
         
         // check if the actors are in the database
         assertTrue(allActors.contains(((Actor) new Actor("A01", "Matthew", "McConaughey", 45))));
@@ -142,27 +124,24 @@ public class MovieTests {
     }
     
     @Test
-    @SuppressWarnings("unchecked")
     public void testGetScreenplays () {
-        List<Screenplay> allScreenplays = session.createQuery("from Screenplay").list();
-        
+        List<Screenplay> allScreenplaysList = createTypedList(db.query(Screenplay.class));
+
         // check if the screenplay is in the database
-        assertTrue(allScreenplays.contains(((Screenplay) new Screenplay("SP01", "Interstallar-2014", Calendar.getInstance(), new Screenwriter("SW01", "Jonathan", "Nolan", 37)))));
+        assertTrue(allScreenplaysList.contains(((Screenplay) new Screenplay("SP01", "Interstallar-2014", Calendar.getInstance(), new Screenwriter("SW01", "Jonathan", "Nolan", 37)))));
     }
     
     @Test
-    @SuppressWarnings("unchecked")
     public void testGetScreenwriters () {
-        List<Screenwriter> allScreenwriters = session.createQuery("from Screenwriter").list();
+    	List<Screenwriter> allScreenwriters = createTypedList(db.query(Screenwriter.class));
         
         // check if the screenwriter is in the database
         assertTrue(allScreenwriters.contains(((Screenwriter) new Screenwriter("SW01", "Jonathan", "Nolan", 37))));
     }
     
     @Test
-    @SuppressWarnings("unchecked")
     public void testGetMovies () {
-        List<Movie> allMovies = session.createQuery("from Movie").list();
+    	List<Movie> allMovies = createTypedList(db.query(Movie.class));
         
         // check if the movies are in the database
         assertTrue(allMovies.contains(((Movie) 
@@ -173,63 +152,61 @@ public class MovieTests {
     }
     
     @Test
-    @SuppressWarnings("unchecked")
     public void testUpdateMovie () {
     	// get the movie
-        List<Movie> moviesQuery = session.createQuery("from Movie M where M.id = 'MINTER'").list();
-        Movie m = moviesQuery.get(0);
+    	List<Movie> allMovies = createTypedList(db.query(Movie.class));
+        Movie m = allMovies.get(0);
         // change the movie title
         m.setTitle("The Room");
-        session.saveOrUpdate(m);
+        db.store(m);
         // check the new title
         assertEquals("The Room", m.getTitle());
         
         // get the movie again
-        List<Movie> moviesQuery2 = session.createQuery("from Movie M where M.id = 'MINTER'").list();
-        Movie m2 = moviesQuery2.get(0);
+        List<Movie> allMovies2 = createTypedList(db.query(Movie.class));
+        Movie m2 = allMovies2.get(0);
         
         // check the title from a new query
         assertEquals("The Room", m2.getTitle());
         
         // and an actor
-        List<Actor> allActors = session.createQuery("from Actor where firstname = 'Jessica'").list();
-        Actor actorJessica = allActors.get(0);
-
+        List<Actor> allActors = createTypedList(db.query(Actor.class));
+        
+        Actor actorJessica = allActors.get(2);
+        
         // check the firstname
         assertEquals("Jessica", actorJessica.getFirstname());
 
         // remove an actor from this movie
         m2.removeActor(actorJessica);
-        session.saveOrUpdate(m2);
+        db.store(m2);
         
         // check the new amount of actors participating
         assertEquals(2, m2.getActors().size());
     }
     
     @Test
-    @SuppressWarnings("unchecked")
     public void testDeleteMovies () {
     	// get the movie
-    	List<Movie> moviesQuery = session.createQuery("from Movie M where M.id = 'MINTER'").list();
-    	Movie m = moviesQuery.get(0);
+    	List<Movie> allMovies = createTypedList(db.query(Movie.class));
+    	Movie m = allMovies.get(0);
     	
     	// remove all actors from it
     	for (Actor a : m.getActors()) {
-			System.out.println(a);
-			session.delete(a);
+			db.delete(a);
 		}
     	
     	// remove the movie
-    	session.delete(m);
+    	db.delete(m);
     	
     	// new movie query
-        moviesQuery = session.createQuery("from Movie M where M.id = 'MINTER'").list();
+    	allMovies = db.queryByExample(classUnderTest);
         
         // check that the movie isn't available anymore in the database
-        assertEquals(0, moviesQuery.size());
+        assertEquals(0, allMovies.size());
         
         // and also the actors
-        List<Actor> allActors = session.createQuery("from Actor").list();
+        List<Actor> allActors = createTypedList(db.query(Actor.class));
         assertEquals(0, allActors.size());
     }
 }
