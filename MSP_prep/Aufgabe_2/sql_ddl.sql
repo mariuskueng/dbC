6f1ca9f0c187fbd5d32073a504abdc50229bcdb8
Marius Küng <apps@mariuskueng.ch>
2015-01-20 15:28:00 +0100

    Aufgabe 2 ii.

--- MSP_prep/Aufgabe_2/sql_ddl.sql
+++ MSP_prep/Aufgabe_2/sql_ddl.sql
@@ -42,9 +42,19 @@ CREATE TABLE Bestellungen_Zutaten (
   FOREIGN KEY (zutaten_id) REFERENCES Zutaten(id)
 );
 
-insert into Zutaten (name, energie, eiweiss, fett, preis) values ("Tomate", "", "", "", 1);
-insert into Kunden (name, handynr, zahlungsform) values ("asdf", "0", "visa");
-insert into Kunden (name, handynr, zahlungsform) values ("asdf", "077 445 83 97", "visa");
-insert into Bestellungen (bestellnummer, preis, abholzeit, naehrwertinformation, kunden_id) values ("B01", 1, CURDATE(), "asdf", 1);
-insert into Bestellungen (bestellnummer, preis, abholzeit, naehrwertinformation, kunden_id) values ("B02", 1, CURDATE(), "asdf", 2);
+-- add data
+insert into Zutaten (name, energie, eiweiss, fett, preis) values ("Tomate", "", "", "", 2.5);
+insert into Zutaten (name, energie, eiweiss, fett, preis) values ("Fleisch", "", "", "", 5);
+insert into Zutaten (name, energie, eiweiss, fett, preis) values ("Brot", "", "", "", 3);
+insert into Zutaten (name, energie, eiweiss, fett, preis) values ("Kaese", "", "", "", 1.5);
+insert into Kunden (name, handynr, zahlungsform) values ("Hans", "0", "visa");
+insert into Kunden (name, handynr, zahlungsform) values ("Peter", "077 445 83 97", "visa");
+insert into Bestellungen (bestellnummer, preis, abholzeit, naehrwertinformation, kunden_id) values ("B01", 5.6, CURDATE(), "asdf", 1);
+insert into Bestellungen (bestellnummer, preis, abholzeit, naehrwertinformation, kunden_id) values ("B02", 10.4, CURDATE(), "asdf", 1);
+insert into Bestellungen (bestellnummer, preis, abholzeit, naehrwertinformation, kunden_id) values ("B03", 8.2, CURDATE(), "asdf", 2);
+insert into Bestellungen_Zutaten (bestellungen_id, zutaten_id) values (1, 1);
+insert into Bestellungen_Zutaten (bestellungen_id, zutaten_id) values (1, 2);
 insert into Bestellungen_Zutaten (bestellungen_id, zutaten_id) values (2, 1);
+insert into Bestellungen_Zutaten (bestellungen_id, zutaten_id) values (2, 2);
+insert into Bestellungen_Zutaten (bestellungen_id, zutaten_id) values (2, 3);
+insert into Bestellungen_Zutaten (bestellungen_id, zutaten_id) values (2, 4);
--- MSP_prep/Aufgabe_2/sql_ql.sql
+++ MSP_prep/Aufgabe_2/sql_ql.sql
@@ -6,9 +6,17 @@ SET SQL_SAFE_UPDATES = 0;
 -- i.
 -- Ein Kunde mit der Handynummer 077 445 83 97 löscht all seine Bestellungem die Tomaten als Zutate haben.
 
-delete b from bestellungen b
+DELETE b FROM bestellungen b
 INNER JOIN Kunden k ON b.kunden_id = k.id
 INNER JOIN Bestellungen_Zutaten bz ON b.id = bz.bestellungen_id
 INNER JOIN Zutaten z ON bz.zutaten_id = z.id
 WHERE k.handynr = "077 445 83 97"
 AND z.name = "Tomate";
+
+-- ii. Schreiben Sie einen SQL-Ausdruck, welcher den Totalpreis aller Bestellungen aller Kunden in der Datenbank ausgibt.
+-- Ihre Lösung soll sich auf Ihre in Teilaufgabe c) definierte Datenbank beziehen.
+
+SELECT k.name , SUM(b.preis) from Bestellungen b
+INNER JOIN Kunden k ON k.id = b.kunden_id
+WHERE k.id = b.kunden_id
+GROUP BY k.name;
